21b15d3924595aa494612f1db9369202b31cc8f2
Emre <emrsmsrli1@gmail.com>
2018-06-26 11:15:42 +0300

    fix castling_permission class being removed but undo class still has a reference

--- Source/Chess/Public/Engine/Undo.h
+++ Source/Chess/Public/Engine/Undo.h
@@ -1,17 +1,16 @@
 #pragma once
 
 #include "Move.h"
-#include "CastlingPermission.h"
 
 namespace engine {
     struct CHESS_API undo {
         move move;
-        castling_permission cast_perm;
+        uint32 cast_perm;
         square en_passant_sq;
         uint32 fifty_move_counter;
         uint64 pos_key;
 
-        undo(const engine::move& move, const castling_permission& cast_perm, const square en_passant_sq,
+        undo(const engine::move& move, const uint32 cast_perm, const square en_passant_sq,
              const uint32 fifty_move_counter, const uint64 pos_key)
             : move(move),
               cast_perm(cast_perm),
