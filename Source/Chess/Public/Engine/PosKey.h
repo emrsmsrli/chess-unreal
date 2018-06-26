317fd9fcc1a0ec5a4d6cf1d2cbb0342f502e084b
Emre <emrsmsrli1@gmail.com>
2018-06-26 14:04:09 +0300

    use defined values instead of hardcoded ones

--- Source/Chess/Public/Engine/PosKey.h
+++ Source/Chess/Public/Engine/PosKey.h
@@ -5,7 +5,7 @@
 
 namespace engine {
     namespace poskey {
-        extern uint64 piece_keys[13][N_BOARD_SQUARES_X];
+        extern uint64 piece_keys[N_PIECES][N_BOARD_SQUARES_X];
         extern uint64 side_key;
         extern uint64 castle_keys[16];
 
