99e87bc0d9f828e0785bf62c5afd8e1c90337057
Emre <emrsmsrli1@gmail.com>
2018-06-27 12:05:21 +0300

    add rank and file mapping arrays

--- Source/Chess/Private/Engine/Transition.cpp
+++ Source/Chess/Private/Engine/Transition.cpp
@@ -5,21 +5,32 @@ namespace engine {
     namespace transition {
         uint32 sq120_sq64[N_BOARD_SQUARES_X];
         uint32 sq64_sq120[N_BOARD_SQUARES];
+
+        uint32 files[N_BOARD_SQUARES_X];
+        uint32 ranks[N_BOARD_SQUARES_X];
     }
 }
 
 void engine::transition::init() {
     for(uint32 i = 0; i < N_BOARD_SQUARES_X; ++i) {
         sq120_sq64[i] = 65;
+
+        files[i] = square::offboard;
+        ranks[i] = square::offboard;
     }
 
     uint32 sq64 = 0;
     for(uint32 r = rank::rank_1; r < rank::rank_none; ++r) {
         for(uint32 f = file::file_a; f < file::file_none; ++f) {
+            // init sq120_sq64, sq64_sq120
             const auto sq = fr_sq120(f, r);
             sq64_sq120[sq64] = sq;
             sq120_sq64[sq] = sq64;
             sq64++;
+            
+            // init files, ranks
+            files[sq] = f;
+            ranks[sq] = r;
         }
     }
 }
@@ -38,4 +49,12 @@ uint32 engine::transition::sq120(const uint32 sq64) {
 
 uint32 engine::transition::sq64(const uint32 sq120) {
     return sq120_sq64[sq120];
+}
+
+engine::rank engine::transition::sq_rank(const uint32 sq) {
+    return static_cast<rank>(ranks[sq]);
+}
+
+engine::file engine::transition::sq_file(const uint32 sq) {
+    return static_cast<file>(files[sq]);
 }
--- Source/Chess/Public/Engine/Transition.h
+++ Source/Chess/Public/Engine/Transition.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Defs.h"
 
 namespace engine {
     namespace transition {
@@ -9,5 +10,9 @@ namespace engine {
         uint32 fr_sq64(uint32 file, uint32 rank);
         uint32 sq120(uint32 sq64);
         uint32 sq64(uint32 sq120);
+
+        // todo do these belong here?
+        engine::rank sq_rank(uint32 sq);
+        engine::file sq_file(uint32 sq);
     }
 }
