e38261f99e1bce98dcc598d0d7b87b700490cfd2
Emre <emrsmsrli1@gmail.com>
2018-06-29 12:58:06 +0300

    change undo::undo() with a simpler version

--- Source/Chess/Public/Engine/Undo.h
+++ Source/Chess/Public/Engine/Undo.h
@@ -10,12 +10,6 @@ namespace engine {
         uint32 fifty_move_counter;
         uint64 pos_key;
 
-        undo(const engine::move& move, const uint32 cast_perm, const square en_passant_sq,
-             const uint32 fifty_move_counter, const uint64 pos_key)
-            : move(move),
-              cast_perm(cast_perm),
-              en_passant_sq(en_passant_sq),
-              fifty_move_counter(fifty_move_counter),
-              pos_key(pos_key) {}
+        undo(const engine::move& m) : move(m) {} 
     };
 }
