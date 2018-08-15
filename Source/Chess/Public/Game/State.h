fa563dae5e8aecdbc50699d282831c055b24f4ad
Emre <emrsmsrli1@gmail.com>
2018-08-15 20:12:28 +0300

    add a game state enumeration

---
+++ Source/Chess/Public/Game/State.h
@@ -0,0 +1,18 @@
+// Copyright 2018 Emre Simsirli
+
+#pragma once
+
+#include "ObjectMacros.h"
+
+UENUM(BlueprintType)
+namespace EState {
+    enum Type {
+        START           UMETA(DisplayName = "Start"),
+        MAIN_MENU       UMETA(DisplayName = "Main Menu"),
+        SEARCH_SERVER   UMETA(DisplayName = "Searching Server"),
+        CONNECT_SERVER  UMETA(DisplayName = "Connecting Server"),
+        LOADING_SCREEN  UMETA(DisplayName = "Loading"),
+        PLAYING         UMETA(DisplayName = "Playing"),
+        ERROR           UMETA(DisplayName = "Error")
+    };
+}
