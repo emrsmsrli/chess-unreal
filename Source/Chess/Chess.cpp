b2e39260e96e272e096df62ea1f75470f1b593ea
Emre <emrsmsrli1@gmail.com>
2018-08-19 22:48:54 +0300

    remove log category and update copyright

--- Source/Chess/Chess.cpp
+++ Source/Chess/Chess.cpp
@@ -1,7 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2018 Emre Simsirli
 
 #include "Chess.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Chess, "Chess" );
-DEFINE_LOG_CATEGORY(LogChess);
--- Source/Chess/Chess.h
+++ Source/Chess/Chess.h
@@ -1,7 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2018 Emre Simsirli
 
 #pragma once
 
 #include "CoreMinimal.h"
-
-DECLARE_LOG_CATEGORY_EXTERN(LogChess, Log, All);
